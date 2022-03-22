cf8695d21def89408aecf35489ec7b79429a15b5
Ғʀᴇᴀ̄ᴋ <98258230+FreakMask@users.noreply.github.com>
2022-03-22 22:12:47 +0530

    Update music.py

--- SiestaRobot/modules/music.py
+++ SiestaRobot/modules/music.py
@@ -248,5 +248,5 @@ async def lyrics_func(_, message):
     lyrics = song.result
     if len(lyrics) < 4095:
         return await m.edit(f"**__{lyrics}__**")
-    lyrics = await paste(lyrics)
+    lyrics = paste(lyrics)
     await m.edit(f"**LYRICS_TOO_LONG:** [URL]({lyrics})")

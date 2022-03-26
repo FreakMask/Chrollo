db74057954e9d8421c1d27d4ee49021dd32f07f2
Ғʀᴇᴀ̄ᴋ <98258230+FreakMask@users.noreply.github.com>
2022-03-26 15:17:24 +0530

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -388,7 +388,7 @@ def siesta_about_callback(update, context):
                     ],
                     [
                         InlineKeyboardButton(text=gs(chat.id, "support_chat_link_button"), callback_data="siesta_support"),
-                        InlineKeyboardButton(text="Credits", callback_data="siesta_credit"),
+                        InlineKeyboardButton(text="Base", callback_data="siesta_credit"),
                     ],
                     [
                         InlineKeyboardButton(text=gs(chat.id, "source_button"), url="https://t.me/TheHunterAssociation"),

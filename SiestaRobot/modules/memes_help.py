ea7fc3c27a207dc9b09e07d7642544c1c65d52ec
Ғʀᴇᴀ̄ᴋ <98258230+FreakMask@users.noreply.github.com>
2022-04-12 12:22:56 +0530

    Create memes_help.py

---
+++ SiestaRobot/modules/memes_help.py
@@ -0,0 +1,8 @@
+__mod_name__ ="Memes"
+
+__help__ ="""
+
+❂ /meme: To get random wholesome memes from subreddit r/wholesomememes.
+
+*Note:*
+❂ This module utilizes meme-api.heroku.com, which uses reddit as a source.
